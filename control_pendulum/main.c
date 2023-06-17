2b55d2e901452021da71e7f93f403f03f0520483
Miguel Marcano <miguel131299@gmail.com>
2023-06-17 16:34:42 +0200

    Add changes to pendulum

--- control_pendulum/main.c
+++ control_pendulum/main.c
@@ -103,6 +103,80 @@ void scroll(GLFWwindow* window, double xoffset, double yoffset)
     mjv_moveCamera(m, mjMOUSE_ZOOM, 0, -0.05*yoffset, &scn, &cam);
 }
 
+//*************************************
+void set_torque_control(const mjModel* m, int actuator_no, int flag)
+{
+
+  if (flag==0)
+    m->actuator_gainprm[10*actuator_no+0] = 0;
+  else
+    m->actuator_gainprm[10*actuator_no+0] = 1;
+
+}
+//***********************************
+
+
+//*************************************
+void set_velocity_servo(const mjModel* m, int actuator_no, double kv)
+{
+  m->actuator_gainprm[10*actuator_no+0] = kv;
+  m->actuator_biasprm[10*actuator_no+2] = -kv;
+}
+//***********************************
+
+
+//*************************************
+void set_position_servo(const mjModel* m, int actuator_no, double kp)
+{
+  m->actuator_gainprm[10*actuator_no+0] = kp;
+  m->actuator_biasprm[10*actuator_no+1] = -kp;
+}
+//***********************************
+
+void mycontroller(const mjModel* m, mjData* d)
+{
+  int i;
+  int actuator_no;
+  //0 = torque actuator
+  actuator_no = 0;
+  int flag = 0;
+  set_torque_control(m, actuator_no, flag);
+  d->ctrl[0] = -10*(d->qpos[0]-0)-1*d->qvel[0];//PD control
+  //d->ctrl[0] = -10*(d->sensordata[0]-0)-1*d->sensordata[1];
+
+  //1=position servo
+  actuator_no = 1;
+  double kp = 0;
+  set_position_servo(m, actuator_no, kp);
+  //for (i=0;i<10;i++)
+  // {
+  //   //printf("%f \n", m->actuator_gainprm[10*actuator_no+i]);
+  //   //printf("%f \n", m->actuator_biasprm[10*actuator_no+i]);
+  // }
+
+  //printf("*********** \n");
+  d->ctrl[1] = 0.5;
+
+  //2= velocity servo
+  actuator_no = 2;
+  double kv = 0;
+  set_velocity_servo(m, actuator_no, kv);
+  d->ctrl[2] = 0.2;
+
+    //PD control
+  actuator_no = 1;
+  double kp2 = 10;
+  set_position_servo(m, actuator_no, kp2);
+  actuator_no = 2;
+  double kv2 = 1;
+  set_velocity_servo(m, actuator_no, kv2);
+  d->ctrl[1] = -0.5;
+  d->ctrl[2] = 0;
+
+
+}
+
+
 
 // main function
 int main(int argc, const char** argv)
@@ -154,13 +228,16 @@ int main(int argc, const char** argv)
     glfwSetMouseButtonCallback(window, mouse_button);
     glfwSetScrollCallback(window, scroll);
 
-    // double arr_view[] = {89.608063, -11.588379, 5, 0.000000, 0.000000, 0.000000};
-    // cam.azimuth = arr_view[0];
-    // cam.elevation = arr_view[1];
-    // cam.distance = arr_view[2];
-    // cam.lookat[0] = arr_view[3];
-    // cam.lookat[1] = arr_view[4];
-    // cam.lookat[2] = arr_view[5];
+    double arr_view[] = {90, -5, 5, 0.012768, -0.000000, 1.254336};
+    cam.azimuth = arr_view[0];
+    cam.elevation = arr_view[1];
+    cam.distance = arr_view[2];
+    cam.lookat[0] = arr_view[3];
+    cam.lookat[1] = arr_view[4];
+    cam.lookat[2] = arr_view[5];
+
+    d->qpos[0]=1.57; //pi/2
+    mjcb_control = mycontroller;
 
     // use the first while condition if you want to simulate for a period.
     while( !glfwWindowShouldClose(window))
